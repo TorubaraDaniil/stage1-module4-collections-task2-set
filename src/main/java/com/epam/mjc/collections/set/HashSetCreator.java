9d1b6ee2188763541c31061c13c3d33c36a51ba2
daniil-torubara <dabarara@bk.ru>
2023-10-05 20:59:18 +0600

    stage 1 module 4 task 2 set

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> newHashSet = new HashSet<>();
+        Iterator<Integer> iter = sourceList.iterator();
+        while(iter.hasNext()){
+            int number = iter.next();
+            if (number % 2 == 0){
+                while (number != 0){
+                    newHashSet.add(number);
+                    number = number / 2;
+                }
+            }
+            else {
+                newHashSet.add(number);
+                newHashSet.add(number * 2);
+            }
+        }
+        return newHashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.set;
 
+import javax.swing.tree.TreeModel;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> newTreeSet = new TreeSet<>();
+        Iterator<Integer> iterator = sourceList.iterator();
+        while (iterator.hasNext()){
+            int number = iterator.next();
+            int square = (int) Math.pow(number, 2);
+            if (square >= lowerBound && square <= upperBound){
+                newTreeSet.add(square);
+            }
+        }
+        return newTreeSet;
     }
 }
